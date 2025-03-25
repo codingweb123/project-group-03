9cc2831d0621b936d506d32d2b8df91244865726
staffm3 <staffm37@gmail.com>
2025-03-25 20:58:47 +0200

    minify js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,1 @@
-document.querySelector('.burger-btn').onclick = e => {
-  e.preventDefault();
-  document.querySelector('.menu-overlay').classList.add('is-open');
-};
-document.querySelector('.button-svg-close').onclick = e => {
-  e.preventDefault();
-  document.querySelector('.menu-overlay').classList.remove('is-open');
-};
+document.querySelector(".burger-btn").onclick=e=>{e.preventDefault(),document.querySelector(".menu-overlay").classList.add("is-open")},document.querySelector(".button-svg-close").onclick=e=>{e.preventDefault(),document.querySelector(".menu-overlay").classList.remove("is-open")};
